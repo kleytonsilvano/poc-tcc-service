f1036cf1bd61344f6c734fa3276e9f97b4f86080
kleytonsilvano <kleyton.silvano@gmail.com>
2021-10-07 22:39:58 -0300

    estrutura projeto

---
+++ src/main/java/tcc/poc/App.java
@@ -0,0 +1,13 @@
+package tcc.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(tcc.poc.App.class, args);
+    }
+
+}

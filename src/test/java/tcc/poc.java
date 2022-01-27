7472a3ddc102832c4fe727d19ecc1327501ffaec
kleytonsilvano <kleyton.silvano@gmail.com>
2022-01-27 18:12:20 -0300

    alteracao teste

---
+++ src/test/java/tcc/poc.java
@@ -0,0 +1,16 @@
+package tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class poc {
+    @SpringBootTest
+    static
+    class DemoApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+}

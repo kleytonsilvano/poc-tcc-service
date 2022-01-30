916bf35448f4b7dc006171bd1ea96c77484f9e92
kleytonsilvano <kleyton.silvano@gmail.com>
2022-01-29 23:46:03 -0300

    cors

---
+++ src/main/java/tcc/poc/filter/DefaultFilter.java
@@ -0,0 +1,42 @@
+package tcc.poc.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DefaultFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+    }
+
+    //Elimina problema de CORS
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, DELETE, PUT, GET, PATCH");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-API-Key, x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, *, x-cpf-customer, x-cnpj-supplier, ");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
